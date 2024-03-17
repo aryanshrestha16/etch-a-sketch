910da29dbf7d4a0baea92e2d07cf5d3994886458
Aryan Shrestha <aryanshrestha3810@gmail.com>
2024-03-16 21:52:53 -0400

    Need to Add Github Icon

---
+++ app.js
@@ -0,0 +1,91 @@
+"use strict"
+const GRID_WIDTH = 500;
+const GRID_HEIGTH = 500;
+let eraser_active = false;
+let rainbow = false;
+let black = true;
+createGrid();
+
+function setEraserTrue(){
+    eraser_active = true;
+    black = false;
+    rainbow = false;
+}
+
+function setBlackTrue(){
+    black = true;
+    rainbow = false;
+    eraser_active = false;
+}
+
+function setRainbowTrue(){
+    rainbow = true;
+    eraser_active = false;
+    black = false;
+}
+
+function createGrid() {
+    removeGrid();
+    const container = document.querySelector(".grid-container");
+    const size = document.querySelector("#grid-value").value;
+    let divWidth = GRID_WIDTH/size;
+    let divHeight = GRID_HEIGTH/size;
+    for (let i = 1; i <= size; i++){
+        const newdiv = document.createElement("div");
+        newdiv.classList.add("row-container");
+        for (let i = 1; i <= size; i++){
+            let columnDiv = document.createElement("div");
+            columnDiv.classList.add("column-div");
+            columnDiv.style.height = `${divHeight}px`;
+            columnDiv.style.width = `${divWidth}px`;
+            newdiv.appendChild(columnDiv);
+        }
+        container.appendChild(newdiv);
+    }
+    updateValue();
+    addHoverEffect();
+}
+
+
+
+function addHoverEffect(){
+    const box = document.querySelectorAll(".column-div");
+    box.forEach((element) => 
+    element.addEventListener("mouseover", () =>{
+        if (black){
+        element.style.backgroundColor = "black";}
+        if (rainbow){
+            let r = Math.random() * 255;
+            let g = Math.random() * 255;
+            let b = Math.random() * 255;
+            element.style.backgroundColor = `rgb(${r},${g},${b})`;
+        }
+        if (eraser_active){
+            element.style.backgroundColor = "white";
+        }
+    }
+        ))
+}
+
+
+function removeGrid(){
+    const rows = document.querySelectorAll(".row-container");
+    rows.forEach((element) => {
+        element.remove();
+    }
+    )
+}
+
+function resetGrid(){
+    const box = document.querySelectorAll(".column-div");
+    box.forEach((element) => {
+        element.style.backgroundColor = "white";
+    }
+    )
+}
+
+function updateValue(){
+    const value = document.getElementById("curr");
+    const size = document.querySelector("#grid-value").value;
+    value.textContent = `Current Grid: ${size}x${size}`;
+}
